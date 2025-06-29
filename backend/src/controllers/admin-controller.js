fed1cbadbc1c791de13ec384f6e41eb810defefb
ayushpanwar2014 <ayush.panwar2014@gmail.com>
2025-06-29 14:09:08 +0530

    backend work zod validation and add doctor+

--- backend/src/controllers/admin-controller.js
+++ backend/src/controllers/admin-controller.js
@@ -1,12 +1,83 @@
+import DoctorModel from "../models/doctor-model.js";
+import { v2 as cloudinary } from 'cloudinary'
+import jwt from 'jsonwebtoken'
 
 
 export const addDoctors = async (req, res, next) => {
 
-    const {name, email, password, speciality, degree, experience, about, available, fees, address} = req.body;
+    const { name, email, password, speciality, degree, experience, about, available, fees, address, slots_booked } = req.body;
+    const imageFile = req.file;
 
     try {
-        
-    } catch (error) {
-        
+
+        // checking if user exist
+        const userExist = await DoctorModel.findOne({ email: email });
+        if (userExist) return res.status(404).json({ success: false, msg: "User already exists!" });
+
+        //image upload in cloudinary
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path, { resource_type: "image" });
+        const imageURL = imageUpload.secure_url;
+
+        //Creating User
+        const createUser = await DoctorModel.create({
+            name: name,
+            email: email,
+            password: password,
+            image: imageURL,
+            speciality: speciality,
+            degree: degree,
+            experience: experience,
+            about: about,
+            available: available,
+            fees: fees,
+            address: address,
+            slots_booked: slots_booked,
+            date: Date.now()
+        });
+
+        res.status(200).json({ success: true, msg: "Doctor Added Successfully!" });
+
+    } catch (err) {
+
+        const error = {
+            status: 401,
+            message: 'Something is Wrong!'
+        };
+        next(error);
+    }
+}
+
+export const loginAdmin = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSSWORD) {
+
+            const token = jwt.sign({
+                email
+            }, process.env.JWT_SECRET,
+                { expiresIn: '1d' }
+            )
+
+            //Send the token in response
+            return res.cookie('adminToken', token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production', // only over HTTPS in prod
+                sameSite: 'Strict', // 'Lax' is OK if frontend/backend are same-origin
+                maxAge: 24 * 60 * 60 * 1000, // 1 day
+            }).status(200).json({success: true, msg: 'Login Successfull!'});
+
+        }
+        else {
+            return res.clearCookie('adminToken').status(400).json({ success: false, msg: "Invalid Credentials!" });
+        }
+
+    } catch (err) {
+
+        const error = {
+            status: 401,
+            message: 'Something is Wrong!'
+        };
+        next(error);
     }
 }
--- backend/src/routes/admin-routes.js
+++ backend/src/routes/admin-routes.js
@@ -1,10 +1,15 @@
 import express from 'express';
-import { addDoctors } from '../controllers/admin-controller.js';
+import { addDoctors, loginAdmin } from '../controllers/admin-controller.js';
 import upload from '../../middlewares/multer.js';
+import { validate } from '../../middlewares/validator-middleware.js';
+import { doctor_signup_schema } from '../zod-validators/doct-auth-validator.js';
 
 
 const Admin_Router = express.Router();
 
-Admin_Router.post('/add-doctor', upload.single('image'), addDoctors)
+Admin_Router.post('/add-doctor', upload.single('image'), validate(doctor_signup_schema), addDoctors);
+Admin_Router.post('/admin-login', loginAdmin);
+
+
 
 export default Admin_Router;
